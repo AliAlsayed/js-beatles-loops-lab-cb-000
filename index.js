32f0dc13ac235a0af6b09be4e187de5cd902b12a
Ali Alsayed <a_alsayed87@hotmail.com>
2019-04-14 09:28:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // add solution here
+
+function theBeatlesPlay(musicians, instruments){
+  var arr = []
+  for (var i = 0; i < musicians.length; i++){
+    arr.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return arr
+}
+
+function johnLennonFacts(facts){
+  var i = 0
+  while i < facts.length{
+    facts[i] += '!!!'
+    i += 1 
+  }
+  return facts
+}
